88124c9e69285449399a27722f9e7f43f8728c6e
KrystianKurlej <101930877+KrystianKurlej@users.noreply.github.com>
2023-08-18 20:51:37 +0200

    footer misspelling

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="max-w-[1800px] px-4 mx-auto py-12 text-center text-sm font-light text-neutral-500">
-        ©2023 Krystian Kurlej - Portfolio<br />Wszelki prawa zastrzeżone.
+        ©2023 Krystian Kurlej - Portfolio<br />Wszelkie prawa zastrzeżone.
     </div>
   );
 };
