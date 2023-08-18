7a1bf4f0b09b1c67186d28681ebd8615c458c528
KrystianKurlej <101930877+KrystianKurlej@users.noreply.github.com>
2023-08-18 18:33:31 +0200

    GA

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Poppins } from 'next/font/google'
 import Footer from './components/Footer';
+import Script from 'next/script';
 
 const poppins = Poppins({
   weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
@@ -22,6 +23,17 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pl">
+      {/* Google tag (gtag.js) */}
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-VKGYSJX0W1" />
+      <Script>
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
+        `}
+      </Script>
       <body className={poppins.className + ` bg-[#020202] text-white`}>
         {children}
         <Footer />
